309e6a18c57e1bb24e387627149cc0dad912f9b4
Pedro Amaral <pedr.amaral@gmail.com>
2015-05-04 14:12:09 +0100

    factory added for mlist

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 angular.module('miniMailGunner', [])
+.factory('mlists', [function(){
+  var o = {
+    mlists: [
+	    {title: 'clients', mails: 'ricardo@ua.pt, irorenne-6658@yopmail.com, jeffippinny-8371@yopmail.com'},
+		  {title: 'publicity', mails: 'jeffippinny-8371@yopmail.com'},
+		  {title: 'online press', mails: 'qafinnudaj-9518@yopmail.com,ricardo@ua.pt, irorenne-6658@yopmail.com'},
+		  {title: 'traditional press', mails: 'qafinnudaj-9518@yopmail.com,ricardo@ua.pt, irorenne-6658@yopmail.com,qafinnudaj-9518@yopmail.com,ricardo@ua.pt, irorenne-6658@yopmail.com,qafinnudaj-9518@yopmail.com,ricardo@ua.pt, irorenne-6658@yopmail.com'},
+		  {title: 'family', mails: 'ennabimiw-2320@yopmail.com,pedr.amaral@gmail.com'}]
+  };
+  return o;
+}])
 .controller('MainCtrl', [
 '$scope',
-function($scope){
+'mlists',
+function($scope, mlists){
   $scope.test = 'Hello world!';
-
+  $scope.mlists = mlists.mlists;
   $scope.addML = function(){
     if(!$scope.title || $scope.title === '' ||
     	 !$scope.mails || $scope.mails === '') { return; }
@@ -16,10 +28,4 @@ function($scope){
       $scope.mails = '';
   };
 
-  $scope.mlists = [
-  {title: 'clients', mails: 'ricardo@ua.pt, irorenne-6658@yopmail.com, jeffippinny-8371@yopmail.com'},
-  {title: 'publicity', mails: 'jeffippinny-8371@yopmail.com'},
-  {title: 'online press', mails: 'qafinnudaj-9518@yopmail.com,ricardo@ua.pt, irorenne-6658@yopmail.com'},
-  {title: 'traditional press', mails: 'qafinnudaj-9518@yopmail.com,ricardo@ua.pt, irorenne-6658@yopmail.com,qafinnudaj-9518@yopmail.com,ricardo@ua.pt, irorenne-6658@yopmail.com,qafinnudaj-9518@yopmail.com,ricardo@ua.pt, irorenne-6658@yopmail.com'},
-  {title: 'family', mails: 'ennabimiw-2320@yopmail.com,pedr.amaral@gmail.com'}];
 }]);
