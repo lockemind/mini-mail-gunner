8e3c53e8e2c8a5db7e3c855ce4d4147064c2c120
Pedro Amaral <pedr.amaral@gmail.com>
2015-05-06 23:19:44 +0100

    error message and initialization issues

--- app/assets/javascripts/mainCtrl.js
+++ app/assets/javascripts/mainCtrl.js
@@ -9,9 +9,11 @@ function($scope, mlists, templates, $http, Auth){
   
   $scope.mlists = mlists.mlists;
   $scope.templates = templates.templates;
-  $scope.mpreview = { title: $scope.mlists[0].title ,mails: $scope.mlists[0].mails};
-  $scope.tpreview = { title: $scope.templates[0].title ,content: $scope.templates[0].content};
-  $scope.message_sent = '';
+  console.log("$scope.mlists.length " + $scope.mlists.length);
+  if ($scope.mlists.length > 0 && $scope.templates.length > 0 ) {
+    $scope.mpreview = { title: $scope.mlists[0].title ,mails: $scope.mlists[0].mails};
+    $scope.tpreview = { title: $scope.templates[0].title ,content: $scope.templates[0].content};
+  }
 
   Auth.currentUser().then(function (user){
     $scope.user = user;
@@ -26,6 +28,7 @@ function($scope, mlists, templates, $http, Auth){
   }
 
   sendM = function() {
+    if(!$scope.tpreview || !$scope.mpreview) { return; }
 
     return $http.post('/msender/send.json', 
       { subject:$scope.tpreview.title,
@@ -34,9 +37,14 @@ function($scope, mlists, templates, $http, Auth){
         user: $scope.user.username,
         email: $scope.user.email
       }).success(function(resp_data){
-      
+        $scope.error_message = null;
         $scope.message_sent = "Message sent.";
-    });
+      }).error(function(data, status, headers, config) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+        $scope.message_sent = null;
+        $scope.error_message = "Error: message not sent.";
+      });
   };
 
   updateMLPreview = function(mlist) {
